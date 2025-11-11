793efe2a7d0e1a83c56c89ae45f90926a6f0cfc4
Vo Huu Phuoc <118438452+Pinnnndz@users.noreply.github.com>
2025-11-11 12:45:53 +0700

    Update tampermonkey.user.js to version 5.3
    Updated the user script to version 5.3, adding 'Ignition Boost' for faster startup and replacing the waiting mechanism with a customizable delay.

---
+++ tampermonkey.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Auto Celebrity PinG ORION+
+// @namespace    http://tampermonkey.net/
+// @version      5.3
+// @description  v5.3 - PinG ORION+: Bổ sung hệ thống "Ignition Boost" để tăng tốc độ khởi động. Loại bỏ cơ chế chờ danh sách ổn định và thay thế bằng một độ trễ khởi động ngắn, có thể tùy chỉnh, giúp script bắt đầu nhanh hơn đáng kể mà vẫn đảm bảo ổn định.
+// @author       Huu Phuoc
+// @require      https://raw.githubusercontent.com/Pinnnndz/locket-autocelebrity-v5/main/auto-celeb.main.js
+// @homepageURL  https://github.com/Pinnnndz/locket-autocelebrity-v3
+// @downloadURL  https://raw.githubusercontent.com/Pinnnndz/locket-autocelebrity-v5/main/tampermonkey.user.js
+// @updateURL    https://raw.githubusercontent.com/Pinnnndz/locket-autocelebrity-v5/main/tampermonkey.user.js
+// @match        https://locket.binhake.dev/*
+// @grant        GM_addStyle
+// @run-at       document-start
+// @icon         https://sf-static.upanhlaylink.com/img/image_20251111ac288b3a07ad34e71f1be21858ab8fe0.jpg
+// ==/UserScript==
